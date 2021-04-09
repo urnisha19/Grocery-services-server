b3d56855a9af480d103fc4044ae9833984d44452
urnisha19 <urnisha09@gmail.com>
2021-04-09 19:08:24 +0600

    updated

--- index.js
+++ index.js
@@ -19,52 +19,51 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzg3r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
     const productCollection = client.db("grocery").collection("products");
     const orderCollection = client.db("grocery").collection("orders");
-    console.log('db connected');
 
     //add product
     app.post('/addProduct', (req, res) => {
         const newProduct = req.body;
-        console.log(newProduct)
         productCollection.insertOne(newProduct)
             .then(result => {
-                console.log('inserted count', result.insertedCount);
                 res.send(result.insertedCount > 0);
             })
     })
 
-    //get product list
+    //get products
     app.get('/products', (req, res) => {
-        productCollection.find()
+        productCollection.find({})
             .toArray((err, items) => {
                 res.send(items)
             })
     })
 
+    //get a product
+    app.get('/checkout/product/:id', (req, res) => {
+        productCollection.find({ id: (req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
     //delete product
     app.delete('/delete/:id', (req, res) => {
-        console.log(req.params.id)
         productCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then(result => {
-                console.log(result)
                 res.send('deleted count: ', result.deletedCount > 0);
             })
     })
 
-
     //add orders
     app.post('/addOrder', (req, res) => {
         const newOrder = req.body;
-        console.log(newOrder)
         orderCollection.insertOne(newOrder)
             .then(result => {
-                console.log('order count', result.insertedCount);
                 res.send(result.insertedCount > 0);
             })
     })
@@ -74,15 +73,12 @@ client.connect(err => {
         const bearer = req.headers.authorization;
         if (bearer && bearer.startsWith('Bearer ')) {
             const idToken = bearer.split(' ')[1];
-            console.log({ idToken });
             admin.auth().verifyIdToken(idToken)
-                .then(function (decodedToken) {
+                .then((decodedToken)=> {
                     const tokenEmail = decodedToken.email;
                     const queryEmail = req.query.email;
-                    console.log(tokenEmail, queryEmail);
-
                     if (tokenEmail == queryEmail) {
-                        productsCollection.find({ email: req.query.email })
+                        orderCollection.find({ email: queryEmail })
                             .toArray((err, documents) => {
                                 res.status(200).send(documents);
                             })
@@ -90,7 +86,8 @@ client.connect(err => {
                     else {
                         res.status(401).send('unauthorized access!!')
                     }
-                }).catch(function (error) {
+                })
+                .catch((error)=> {
                     res.status(401).send('unauthorized access!!')
                 });
         }
@@ -112,15 +109,14 @@ client.connect(err => {
         console.log(req.params.id)
         orderCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then(result => {
-                console.log(result)
                 res.send('deleted count: ', result.deletedCount > 0);
             })
     })
 });
+
 // connection check
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
 
 app.listen(process.env.PORT || port)
